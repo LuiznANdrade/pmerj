6e4f73e71562651648093f836a851f5adc1b66b2
Luis Andrade <79378948+LuiznANdrade@users.noreply.github.com>
2023-07-30 18:36:16 -0300

    Primeira Atualização

---
+++ config.js
@@ -0,0 +1,9 @@
+// MUDE TUDO QUE QUISER NO PORTAL DA PMERJ
+// NO AVISO EM PARAGRAFO PARA PULAR LINHA COLOQUE UM <BR>
+var tituloAviso = "AVISO AQUI"
+var aviso = `
+`;
+
+
+var textTiluto = document.getElementById('textTiluto').innerHTML = tituloAviso;
+var paragrafos = document.getElementById('paragrafos').innerHTML= aviso;
