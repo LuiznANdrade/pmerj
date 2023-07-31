9641fd1c6ca8d8818319b4340137f63254feff2b
Luis Eduardo Andrade <79378948+LuiznANdrade@users.noreply.github.com>
2023-07-30 22:06:26 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,10 @@
 // MUDE TUDO QUE QUISER NO PORTAL DA PMERJ
 // NO AVISO EM PARAGRAFO PARA PULAR LINHA COLOQUE UM <BR>
-var tituloAviso = "AVISO AQUI"
-var aviso = `
+var tituloAviso = " POLÍCIA MILITAR DO ESTADO DO RIO DE JANEIRO - PMERJ <br><br> VÍDEOS TUTORIAIS"
+var aviso = `COMO REALIZAR PRISÕES:<br>
+https://www.youtube.com/watch?v=b0Wwo1gceBE&ab_channel=PMERJ-NEXUSRJ <br><br> 
+COMO DETER UM VEÍCULO: <br>
+https://www.youtube.com/watch?v=RyUko8JTOyg&ab_channel=PMERJ-NEXUSRJ <br><br> 
 `;
 
 
